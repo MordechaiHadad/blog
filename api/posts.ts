11ca91ef9f9769c017251ba7bc3d895553243e1c
morde <mordechai.hadad01@gmail.com>
2024-10-27 16:39:09 +0200

    dwadawd

--- api/posts.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-    const filePath = path.join(process.cwd(), 'src', 'lib', 'posts', 'post1.svx');
-    const content = fs.readFileSync(filePath, 'utf-8');
-    res.status(200).send(content);
-}
