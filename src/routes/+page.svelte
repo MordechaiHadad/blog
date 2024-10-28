1057fac2d43b18c514e1e6ecb499382622e67a30
morde <mordechai.hadad01@gmail.com>
2024-10-28 12:37:32 +0200

    what

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Home from '$lib/pages/Home.svelte';
 
 	let { data } = $props();
+	console.log(data.posts[0].slug);
 </script>
 
 <Home posts={data.posts} />
--- src/routes/articles/[slug]/+page.server.ts
+++ src/routes/articles/[slug]/+page.server.ts
@@ -1,6 +1,3 @@
-import { error } from '@sveltejs/kit';
-import { read } from '$app/server';
-
 export async function load({ params }) {
 	return {
 		slug: params.slug
