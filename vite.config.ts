d5bda1075979fe0dfcd56b061f0c857e3bf65409
morde <mordechai.hadad01@gmail.com>
2024-10-28 12:40:14 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		fs: { allow: ['..'] }
-	},
+    server: {
+        fs: {
+            allow: [path.resolve('posts')]
+        }
+    },
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
