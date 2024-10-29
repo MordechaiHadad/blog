f93a2f9f9b30ff64856a4f2d228a7b5d5404ea55
morde <mordechai.hadad01@gmail.com>
2024-10-29 12:14:28 +0200

    added: on click outside for toc

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -9,17 +9,20 @@
 	}
 
 	let { post }: Props = $props();
-
-	let context: IContext = getContext('context');
+	let isImageLoaded = $state(false);
 </script>
 
-<a
-	class="flex flex-col"
-	href="/articles/{post.slug}"
-
->
+<a class="flex flex-col" href="/articles/{post.slug}">
 	<div class="relative">
-		<img src={post.image} alt="{post.slug} cover" class="rounded-2xl" />
+		<img
+			src={post.image}
+			alt="{post.slug} cover"
+			class="rounded-2xl"
+			onload={() => (isImageLoaded = true)}
+		/>
+		<!-- {#if !isImageLoaded}
+			<div class="size-full" ></div>			
+		{/if} -->
 		<p
 			class="absolute left-2.5 top-2.5 z-10 rounded-2xl bg-gray-200 px-3 py-1.5 text-sm dark:bg-zinc-700"
 		>
@@ -27,7 +30,9 @@
 		</p>
 	</div>
 	<div class="flex flex-col gap-1">
-		<div class="flex place-items-center gap-1.5 text-start text-xs text-neutral-600 dark:text-neutral-400">
+		<div
+			class="flex place-items-center gap-1.5 text-start text-xs text-neutral-600 dark:text-neutral-400"
+		>
 			<p>
 				{post.date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}
 			</p>
@@ -35,6 +40,8 @@
 			<p>{calculateReadingTime(post.content)} minutes read</p>
 		</div>
 		<h2 class="text-start text-lg font-bold">{post.title}</h2>
-		<p class="line-clamp-2 text-start text-sm text-neutral-600 dark:text-neutral-400">{post.description}</p>
+		<p class="line-clamp-2 text-start text-sm text-neutral-600 dark:text-neutral-400">
+			{post.description}
+		</p>
 	</div>
 </a>
--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { clickOutside } from '$lib/actions/clickOutside.js';
 	import Post from '$lib/pages/Post.svx';
 	import { onMount } from 'svelte';
 	import { ChevronRight, ChevronLeft } from 'svelte-feathers';
@@ -13,6 +14,7 @@
 		<div
 			class="fixed right-0 top-[42%] z-10 flex flex-col gap-4 rounded-md border border-gray-200 bg-neutral-100 p-4 shadow-md dark:border-zinc-700 dark:bg-neutral-900"
 			transition:fly={{ x: 50, duration: 500 }}
+			use:clickOutside={() => (isTableOfContentsVisible = false)}
 		>
 			<button class="-translate-x-2" onclick={() => (isTableOfContentsVisible = false)}>
 				<ChevronRight class="size-6" />
