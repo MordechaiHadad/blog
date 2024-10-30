51e40118dfcd98b8bc26cbd58e59d51d2089aa9c
morde <mordechai.hadad01@gmail.com>
2024-10-30 11:55:19 +0200

    add: display content for posts in rss feed

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,18 +1,28 @@
-import { getPosts } from "$lib/posts";
+import { getPosts } from '$lib/posts';
+import { compile } from 'mdsvex';
 
 export async function GET() {
 	const posts = await getPosts();
 
-	const headers = { 'Content-Type': 'application/xml' }
+	const headers = { 'Content-Type': 'application/xml' };
+
+	const processedPosts = await Promise.all(
+		posts.map(async (post) => {
+			if (!post.content || post.content.length === 0) return post;
+			const result = await compile(post.content);
+			const code = result?.code!;
+			return { ...post, content: code };
+		})
+	);
 
 	const xml = `
-		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+		<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
 			<channel>
 				<title>Mordechai's Blog</title>
 				<description>It's just a blog bruzzer</description>
 				<link>https://mord-blog.vercel.app/</link>
 				<atom:link href="https://mord-blog.vercel.app/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
+				${processedPosts
 					.map(
 						(post) => `
 						<item>
@@ -21,13 +31,14 @@ export async function GET() {
 							<link>https://mord-blog.vercel.app/articles/${post.slug}</link>
 							<guid isPermaLink="true">https://mord-blog.vercel.app/articles/${post.slug}</guid>
 							<pubDate>${post.date.toUTCString()}</pubDate>
+                            <content:encoded><![CDATA[${post.content}]]></content:encoded>
 						</item>
 					`
 					)
 					.join('')}
 			</channel>
 		</rss>
-	`.trim()
+	`.trim();
 
-	return new Response(xml, { headers })
+	return new Response(xml, { headers });
 }
