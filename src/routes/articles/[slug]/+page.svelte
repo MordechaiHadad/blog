ee34edd4f3d0801ea197715983d59f7f29504810
morde <mordechai.hadad01@gmail.com>
2024-12-02 19:54:58 +0200

    idk

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -6,7 +6,6 @@
 
 	let { data } = $props();
 	let isTableOfContentsVisible = $state(false);
-
 </script>
 
 <svelte:head>
@@ -20,9 +19,13 @@
 			class="fixed right-0 top-[42%] z-10 flex flex-col gap-4 rounded-md border border-gray-200 bg-neutral-100 p-4 shadow-md dark:border-zinc-700 dark:bg-neutral-900"
 			transition:fly={{ x: 50, duration: 500 }}
 			use:clickOutside={{ callback: () => (isTableOfContentsVisible = false), allowSwipe: true }}
-            aria-label="Table of contents"
+			aria-label="Table of contents"
 		>
-			<button class="-translate-x-2" onclick={() => (isTableOfContentsVisible = false)} aria-label="Close table of contents">
+			<button
+				class="-translate-x-2"
+				onclick={() => (isTableOfContentsVisible = false)}
+				aria-label="Close table of contents"
+			>
 				<ChevronRight class="size-6" />
 			</button>
 			<h4 class="size-lg font-bold">Table Of Contents</h4>
@@ -30,7 +33,9 @@
 				class="~max-h-40/max-h-80 flex flex-col gap-2 overflow-y-auto text-wrap p-1 ~max-w-48/60"
 			>
 				{#each data.headers.filter((header) => header.level === 2) as header, i}
-					<a class="hover:font-bold" href="#{header.id}" aria-label="Jump to section: {header.text}">{i}. {header.text}</a>
+					<a class="hover:font-bold" href="#{header.id}" aria-label="Jump to section: {header.text}"
+						>{i}. {header.text}</a
+					>
 				{/each}
 			</div>
 		</nav>
@@ -39,8 +44,8 @@
 			class="fixed right-0 top-[42%] z-10 rounded-md border border-gray-200 bg-neutral-100 p-1.5 shadow-md dark:border-zinc-700 dark:bg-neutral-900"
 			transition:fly={{ x: 50, duration: 500 }}
 			onclick={() => (isTableOfContentsVisible = true)}
-            aria-label="Open table of contents"
-            aria-expanded={isTableOfContentsVisible}
+			aria-label="Open table of contents"
+			aria-expanded={isTableOfContentsVisible}
 		>
 			<ChevronLeft class="size-6" />
 		</button>
