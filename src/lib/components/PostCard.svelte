9fe201de573a403f09c966931ab3c80a7bdfb5de
MordechaiHadad <mordechai.hadad01@gmail.com>
2025-10-17 16:45:18 +0300

    Enhance PostCard component layout with aspect ratio and image styling adjustments

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -12,11 +12,11 @@
 </script>
 
 <a class="flex flex-col gap-0.5" href="/articles/{post.slug}">
-	<div class="relative" aria-labelledby="post-{post.slug}">
+	<div class="relative aspect-[4/3] overflow-hidden rounded-2xl" aria-labelledby="post-{post.slug}">
 		<img
 			src={post.image}
 			alt="{post.slug} cover"
-			class="rounded-2xl max-h-[222.5px]"
+			class="size-full object-cover"
 		/>
 		<p
 			class="absolute left-2.5 top-2.5 z-10 rounded-2xl bg-gray-200 px-3 py-1.5 text-sm dark:bg-zinc-700"
